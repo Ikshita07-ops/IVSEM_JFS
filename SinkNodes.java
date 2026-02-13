96bca519bf5cf51178b20b49a06b85aedc5c8b6b
Ikshita07-ops <rikshita08@gmail.com>
2026-02-13 11:25:38 +0530

    sink static'

---
+++ SinkNodes.java
@@ -0,0 +1,24 @@
+public class SinkNodes {
+    public static void main(String[] args) {
+        int graph[][] = { {0,1,1,0,1} , {0,0,0,1,0} , {0,0,0,1,1} , {0,0,0,0,0} , {0,0,0,0,0} };
+        int zero_ctr = 0;
+        int sink_ctr = 0;
+        for(int i = 0; i < graph.length; i++)
+        {
+            zero_ctr = 0;
+            for(int j = 0; j < graph.length; j++)
+            {
+                if(graph[i][j] == 0) {
+                    zero_ctr++;
+                }
+                }
+                if(zero_ctr == graph.length){
+                    System.out.println("Sink node = " + (char)(65+i)); //type casting from int to char
+                    sink_ctr++;
+                }
+            }
+            System.out.println("Number of Sink nodes = " + sink_ctr);
+        }
+
+    }
+
